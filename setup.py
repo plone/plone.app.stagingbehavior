7d59b1a0753b195f93121b5efbd5951331a07ec6
Jonas Baumann <j.baumann@4teamwork.ch>
2009-10-22 07:11:46 +0000

    created package with paster
    svn path=/plone.stagingbehavior/trunk/; revision=30751

---
+++ setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup, find_packages
+import os
+
+version = '0.1a1'
+
+setup(name='plone.stagingbehavior',
+      version=version,
+      description="Provides a behavior for using plone.app.iterate with dexterity content types",
+      long_description=open("README.txt").read() + "\n" +
+                       open(os.path.join("docs", "HISTORY.txt")).read(),
+      # Get more strings from http://www.python.org/pypi?%3Aaction=list_classifiers
+      classifiers=[
+        "Programming Language :: Python",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        ],
+      keywords='plone dexterity behavior iterate staging',
+      author='Jonas Baumann',
+      author_email='mailto:dexterity-development@googlegroups.com',
+      url='http://plone.org/products/dexterity',
+      license='GPL',
+      packages=find_packages(exclude=['ez_setup']),
+      namespace_packages=['plone'],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          'setuptools',
+          # -*- Extra requirements: -*-
+      ],
+      entry_points="""
+      # -*- Entry points: -*-
+      """,
+      )

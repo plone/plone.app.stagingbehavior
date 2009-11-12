996a1188b81a8d37faf3d505671d0658bc284e60
Jonas Baumann <j.baumann@4teamwork.ch>
2009-11-12 13:18:35 +0000

    improved info viewlet
    svn path=/plone.stagingbehavior/trunk/; revision=31327

--- plone/stagingbehavior/browser/info.py
+++ plone/stagingbehavior/browser/info.py
@@ -7,6 +7,7 @@ from plone.app.layout.globals.interfaces import IViewView
 from plone.app.layout.viewlets.interfaces import IAboveContent
 from plone.memoize.instance import memoize
 
+from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
 from plone.stagingbehavior.interfaces import IStagingSupport
 from plone.stagingbehavior.utils import get_baseline, get_working_copy, get_checkout_relation
 
@@ -18,10 +19,11 @@ class BaselineInfoViewlet( info.BaselineInfoViewlet, grok.Viewlet ):
     grok.context( IStagingSupport )
     grok.implements( IViewView )
 
+    template = ViewPageTemplateFile('info_baseline.pt')
 
     def render(self):
-        if IBaseline.providedBy(self.context):
-            return info.BaselineInfoViewlet.render(self)
+        if IBaseline.providedBy(self.context) and self.working_copy() is not None:
+            return self.template()
         return ''
 
     def _getReference( self ):
@@ -32,6 +34,18 @@ class BaselineInfoViewlet( info.BaselineInfoViewlet, grok.Viewlet ):
     def working_copy( self ):
         return get_working_copy( self.context )
 
+    def creator(self):
+        local_roles = self.working_copy().get_local_roles()
+        if len(local_roles)==1:
+            user_id = local_roles[0][0]
+            return self.context.portal_membership.getMemberById(user_id)
+        else:
+            return info.BaseInfoViewlet.creator(self)
+
+    @property
+    def linked_working_copy(self):
+        member = self.context.portal_membership.getAuthenticatedMember()
+        return member.getId() == self.creator().getId()
 
     @property
     @memoize

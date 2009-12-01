fcc445ffdc884e0f54614c3446c2e9abe0ce23e6
Jonas Baumann <j.baumann@4teamwork.ch>
2009-12-01 07:56:06 +0000

    added method for easy getting the relations of a object
    svn path=/plone.stagingbehavior/trunk/; revision=32169

--- plone/stagingbehavior/relation.py
+++ plone/stagingbehavior/relation.py
@@ -1,7 +1,10 @@
 
 from persistent.dict import PersistentDict
+from zc.relation.interfaces import ICatalog
+from zope.app.intid.interfaces import IIntIds
 from zope.interface import implements
 from zope.annotation.interfaces import IAttributeAnnotatable
+from zope.component import getUtility
 
 from z3c.relationfield import relation
 
@@ -10,7 +13,25 @@ from plone.stagingbehavior.interfaces import IStagingRelationValue
 class StagingRelationValue(relation.RelationValue):
     implements(IStagingRelationValue, IAttributeAnnotatable)
 
+    @classmethod
+    def get_relations_of( cls, obj, from_attribute=None ):
+        """ a list of relations to or from the passed object
+        """
+        catalog = getUtility( ICatalog )
+        intids = getUtility( IIntIds )
+        obj_id = intids.getId( obj )
+        items = list( catalog.findRelations({
+                    'from_id' : obj_id,
+                    }) )
+        items += list( catalog.findRelations({
+                    'to_id' : obj_id,
+                    }))
+        if from_attribute:
+            condition = lambda r:r.from_attribute==from_attribute and \
+                not r.is_broken()
+            items = filter( condition, items )
+        return items
+
     def __init__(self, to_id):
         super(StagingRelationValue, self).__init__(to_id)
         self.staging_properties = PersistentDict()
-
